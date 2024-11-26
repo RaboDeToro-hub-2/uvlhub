709f914685fd2aaaf01d3edf4a96881e11aea864
Marco Padilla Gómez <marco.padilla.gomez@gmail.com>
2024-11-26 14:02:49 +0100

    feat: orcid_id added to models

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(256), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-
+    orcid_id = db.Column(db.String(19), unique=True, nullable=True)
     data_sets = db.relationship('DataSet', backref='user', lazy=True)
     profile = db.relationship('UserProfile', backref='user', uselist=False)
     active = db.Column(db.Boolean, default=True, nullable=False)
